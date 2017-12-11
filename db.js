9f5a9012084b4be55231af7d4b3173c0bbc556c1
Bruno Mendoza <hola@brunomendoza.es>
2017-12-11 15:04:55 +0000

    Deprecated methods fixed

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://ikea:ikea12345@ds129156.mlab.com:29156/ikea-rest-api');
+mongoose.connect('mongodb://root:12345abc@ds129156.mlab.com:29156/ikea-rest-api', {useMongoClient: true,});
 
